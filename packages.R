b4e233c5304c185f4dc36b72647a652335204f65
DataWojtek <witzemacher.master@gmail.com>
2025-02-04 12:39:11 +0100

    added packages.R
    This file installs the missing packages and imports the required R-packages needed for this project

---
+++ packages.R
@@ -0,0 +1,18 @@
+# packages.R - this file imports and installs if needed the required packages
+#              for this project
+
+# vector of the required packages of this project
+required_packages <- c()
+
+# checks which packages are already installed
+new_packages <- required_packages[!(required_packages %in% 
+                                    installed.packages()[,"Package"])]
+
+# installs the new packages if there are any
+if(length(new_packages)) 
+{
+  install.packages(new_packages)
+}
+
+# imports the required packages
+lapply(required_packages, library, character.only = TRUE)
