fe55b1439910df25e2794f755f8613c967846fc9
DataWojtek <witzemacher.master@gmail.com>
2025-02-06 10:43:47 +0100

    Created the frame for 2 a)
    Created a file containing indicating comments for the position of each minor task.

---
+++ scripts/2_1.R
@@ -0,0 +1,38 @@
+# make sure to set the working directory to the 'Titanic-Group-B' project folder
+# Task 2.1 / 2 a)
+
+# import and install required packages
+source("packages.R")
+
+
+# load the processed dataframe
+load("data/processed_titanic.RData")
+
+
+
+### 2.1.1 - Descriptive statistics for metric variables
+
+
+
+### 2.1.2 - Descriptive statistics for categorial variables
+
+
+
+### 2.1.3 - Descriptive statistic for the relationship between two categorial
+###         variables
+
+
+
+### 2.1.4 - Descriptive statistic for the relationship between a metric and a
+###         dichotomous variable
+
+
+
+### 2.1.5 - Visualization for three or four categorial variables
+
+
+
+### 2.1.6 - Optional: Additional functions for descriptive statistics or 
+###         visualizations
+
+
