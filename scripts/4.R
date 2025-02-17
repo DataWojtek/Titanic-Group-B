a808716a13ca6cd0ea769ed65084c5cce9533b35
peijunyee <peijun.yee@tu-dortmund.de>
2025-02-17 11:10:22 +0100

    Update script 4
    Co-Authored-By: ganeisraaj <192605284+ganeisraaj@users.noreply.github.com>
    Co-Authored-By: iqmanadhirah <191980296+iqmanadhirah@users.noreply.github.com>

--- scripts/4.R
+++ scripts/4.R
@@ -2,12 +2,17 @@
 # Task 4
 
 # import and install required packages
-source("packages.R")
+source("~/GitHub/Titanic-Group-B/packages.R")
 
 
 # load the processed dataframe
-load("data/processed_titanic.RData")
+load("~/GitHub/Titanic-Group-B/data/processed_titanic.RData")
+
+source("~/GitHub/Titanic-Group-B/scripts/2_1.R")
 
 
-
-### Task 4
+# Convert necessary columns to factors (categorical variables)
+titanic$Survived <- as.factor(titanic$Survived)
+titanic$Pclass <- as.factor(titanic$Pclass)
+titanic$Sex <- as.factor(titanic$Sex)
+titanic$Embarked <- as.factor(titanic$Embarked)
