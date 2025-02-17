8bcaf2eef1f70974520beb497e38f2848d6c01b9
ganeisraaj <ganeisraajkathiravan@gmail.com>
2025-02-17 19:11:19 +0100

    Update 4.R
    code for the descriptive analysis and statistics parts

--- scripts/4.R
+++ scripts/4.R
@@ -21,3 +21,30 @@ titanic$Survived <- as.factor(titanic$Survived)
 titanic$Pclass <- as.factor(titanic$Pclass)
 titanic$Sex <- as.factor(titanic$Sex)
 titanic$Embarked <- as.factor(titanic$Embarked)
+
+### --- Descriptive Statistics for Metric Variables ---
+cat("\n--- Descriptive Statistics for Metric Variables ---\n")
+metric()
+
+### --- Descriptive Statistics for Categorical Variables ---
+cat("\n--- Descriptive Statistics for Categorical Variables ---\n")
+describe_categorical_vars()
+
+### --- Relationship Between Two Categorical Variables ---
+cat("\n--- Relationship: Survival by Sex ---\n")
+categorical_relationship("Sex", "Survived")
+
+cat("\n--- Relationship: Survival by Passenger Class ---\n")
+categorical_relationship("Pclass", "Survived")
+
+cat("\n--- Relationship: Survival by Embarked Location ---\n")
+categorical_relationship("Embarked", "Survived")
+
+### --- Relationship Between a Metric and a Dichotomous Variable ---
+cat("\n--- Testing Age Difference Between Survivors and Non-Survivors ---\n")
+age_difference <- stat_age_survived()
+if (age_difference) {
+    print("There is a significant difference in age between survivors and non-survivors.")
+} else {
+    print("No significant difference in age between survivors and non-survivors.")
+}
